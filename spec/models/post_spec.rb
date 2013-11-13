4adc2bd33561958615bbb46e9ada0294a5c3a0c1
Kim Daekwon <propellerheaven@gmail.com>
2013-11-13 11:14:02 +0900

    Add rspec configure and sample test files

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+require 'spec_helper'
+
+describe Post do
+  it "First Test" do
+    post = FactoryGirl.build(:post)
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,24 +1,20 @@
 require 'middleman'
+require 'database_cleaner'
 require 'capybara/rspec'
-
 require 'coveralls'
-Coveralls.wear!
-
 require 'factory_girl'
-require 'factory_girl_rails'
-
 require 'rspec/autorun'
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+require './config/boot.rb'
+Dir[File.join("./spec/factories**/*.rb")].each {|file| require file }
+
+# Coveralls
+Coveralls.wear!
 
 RSpec.configure do |config|
   config.before(:suite) { DatabaseCleaner.strategy = :truncation }
   config.before(:each) { DatabaseCleaner.start }
   config.after(:each) { DatabaseCleaner.clean }
-
-  config.fixture_path = "/spec/fixtures"
-  config.use_transactional_fixtures = true
-  config.infer_base_class_for_anonymous_controllers = false
-
   config.order = "random"
   config.include FactoryGirl::Syntax::Methods
 end
