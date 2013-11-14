7b657bdd87a0d114a845b3cf9dba5cd8ecf5f725
Kim Daekwon <propellerheaven@gmail.com>
2013-11-14 16:48:38 +0900

    Config relative_path

--- config.rb
+++ config.rb
@@ -17,9 +17,10 @@ helpers PresentationHelper
 
 # Directory configure
 sprockets.append_path File.join 'app', 'assets', 'javascripts'
-set :css_dir, './stylesheets'
-set :js_dir, './javascripts'
-set :images_dir, './images'
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
+set :http_prefix, ''
 
 # Markdown configure
 set :markdown_engine, :redcarpet
@@ -27,5 +28,27 @@ set :markdown, :fenced_code_blocks => true, :smartypants => true
 
 # Build-specific configuration
 configure :build do
+  # For example, change the Compass output style for deployment
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+
+  # Enable cache buster
+  # activate :cache_buster
+
+  # Use relative URLs
+  activate :relative_assets
+
+  # Compress PNGs after build
+  # First: gem install middleman-smusher
+  # require "middleman-smusher"
+  # activate :smusher
+
+  # Or use a different image path
+    # set :http_prefix, "/Content/images/"
+
+  # set :http_prefix, "./"
+
 end
 
