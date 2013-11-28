2ef36d39d2a5cb72ecd2f45e6cc9baa8ae78d356
Kim Daekwon <propellerheaven@gmail.com>
2013-11-28 16:49:06 +0900

    Update controllers

--- app/controllers/hangout_controller.rb
+++ app/controllers/hangout_controller.rb
@@ -1,5 +1,17 @@
 class HangoutController < MiddlemanController
   def index
-    @hangouts = YAML.load_file(ENV['MM_ROOT'] + "/data/hangouts.yaml")
+    @hangouts = load_hangouts
+  end
+
+  def hangouts_proxy
+    proxy_collection load_hangouts, "#target#", "/hangouts/template.html", :hangout,  :ignore => true
+  end
+
+  private
+  def load_hangouts
+    hangouts = YAML.load_file(ENV['MM_ROOT'] + "/data/hangouts.yaml")
+    hangouts = hangouts.delete_if { |hangout| hangout['date'] == nil}
+    hangouts = hangouts.map {|hangout| HangoutDecorator.new(hangout) }
+    hangouts.sort { |a, b| b['date'] <=> a['date'] }
   end
 end
