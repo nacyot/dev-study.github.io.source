71ca4acb8d810750fb944f05fafe815865cdc289
Kim Daekwon <propellerheaven@gmail.com>
2013-11-28 16:51:56 +0900

    Update helpers

--- app/helpers/presentation_helper.rb
+++ app/helpers/presentation_helper.rb
@@ -1,12 +1,12 @@
 module PresentationHelper
-  def embed_presentation(url = "", width = 427, height = 356)
+  def embed_presentation(url = "", width = 580, height = 480)
     case url
     when /slid\.es/
       slid_dot_es(url, width, height)
     when /slideshare/
       slideshare(url, width, height)
     else
-      "No embed"
+      "<i class='fa fa-external-link'></i> <a href='#{url}'>Presentation Slide</a>"
     end
   end
 
@@ -22,19 +22,22 @@ EOS
 EOS
   end
   
-  def slid_dot_es(slide_url, width, height)
+  def slid_dot_es(url, width, height)
     return <<EOS
 <iframe class="slid_dot_es" src="#{ url }/embed" width="#{ width }" height="#{ height }" scrolling="no" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
 EOS
   end
   
-  def slideshare(slide_url, width, height)
-    url = "http://www.slideshare.net/api/oembed/2?url=#{ slide_url }&format=json"
+  def slideshare(url, width, height)
+    url = "http://www.slideshare.net/api/oembed/2?url=#{ url }&format=json"
     HTTParty.get(url, :format => "json").parsed_response =~ /embed_code\/([0-9]*)/
     slide_number = $1
     
     return <<EOS
 <iframe src="http://www.slideshare.net/slideshow/embed_code/#{ slide_number }" width="#{ width }" height=" #{ height } " frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC;border-width:1px 1px 0;margin-bottom:5px" allowfullscreen> </iframe>
 EOS
+    rescue
+    retrun url
   end
+  
 end
